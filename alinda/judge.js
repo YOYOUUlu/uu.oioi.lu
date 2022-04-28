a048e4d4587ac86e372f8db6b51e975cb6098214
YOYOUU <89437971+YOYOUUlu@users.noreply.github.com>
2022-04-28 15:34:27 +0800

    Update judge.js

--- alinda/judge.js
+++ alinda/judge.js
@@ -4,7 +4,7 @@ function judge() {
     if (username == "LoveUAlinda") {
      window.location.href = "love.html"
     }else if(unsername == "jackyfzh" || unsername == "Jackyfzh" || unsername == "fzh") {
-      window.location.href = "love.html"
+      window.location.href = "buddy.html"
     }else {
         alert("给 爷 爬！")
         event.preventDefault();
