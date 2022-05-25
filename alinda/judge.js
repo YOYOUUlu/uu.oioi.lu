0efbb43ab5001ff0c5aa19016b858d609b6afad5
yoyouulu <yoyouujerry@163.com>
2022-05-25 09:20:09 +0800

    update

--- alinda/judge.js
+++ alinda/judge.js
@@ -1,11 +1,17 @@
-function getValue(id) {
+function getValue(id) 
+{
         return document.getElementById(id).value;
+}
+function checkuser() 
+{
+    if(getValue('uname') == "alinda" && getValue('pwd') == "20090223") 
+    {
+        return true;
     }
-    function checkuser() {
-        if(getValue('uname') == "alinda" && getValue('pwd') == "20090223") {
-            return true;
-        }else {
-            alert("用户名：名字 密码：生日")
-            return false;
-        }
+    else 
+    {
+        alert("用户名：名字 密码：生日")
+        return false;
+        event.preventDefault();
+    }
     }
